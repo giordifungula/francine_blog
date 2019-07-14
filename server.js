57ea44ea32a6df78ccf19fc6c9279626e60f5ed7
giordifungula <jodi.fungula@gmail.com>
2019-07-14 15:40:30 +0200

    template engine

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ const express = require("express");
 const app = new express();
 
 const path = require("path");
+
+const expressEdge = require("express-edge");
+// expressEdge will use template engine
 const router = express.Router();
 
 app.use(express.static("public"));
 // load the static pages
+app.use(expressEdge);
+app.set("views", __dirname + "/views");
+
+// // Requests will be here
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+// });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
+
 // Renders the home page
 app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/about.html"));
