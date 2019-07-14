af0e63dfd77f367a97cce9edacdeec31024a4b1b
giordifungula <jodi.fungula@gmail.com>
2019-07-14 15:31:20 +0200

    additional pages added to website

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/index.html"));
 });
 // Renders the home page
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+// render the about page
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+// render the contact page
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+// render the post page
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
