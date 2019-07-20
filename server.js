c4e5607dfd0abde66a4f76ed9e460e5d76339679
giordifungula <jodi.fungula@gmail.com>
2019-07-20 15:02:50 +0200

    linking to the background images

--- server.js
+++ server.js
@@ -1,88 +1,73 @@
+const path = require("path");
+const expressEdge = require("express-edge");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+const Post = require("./database/models/Post");
 
 const app = new express();
 
-const path = require("path");
-
-const expressEdge = require("express-edge");
-// expressEdge will use template engine
-const mongoose = require("mongoose");
-// mongoose data base
-const router = express.Router();
-
-const bodyParser = require("body-parser");
-// body parser to collect data from forms
-const Post = require("./database/models/Post");
-
-const fileUpload = require("express-fileupload");
-// will allow us to upload files
+mongoose
+  .connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true })
+  .then(() => "You are now connected to Mongo!")
+  .catch(err => console.error("Something went wrong", err));
 
 app.use(fileUpload());
 app.use(express.static("public"));
-// load the static pages
 app.use(expressEdge);
 app.set("views", __dirname + "/views");
-// will render the routing pages
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
-// setting up body parser
+
 app.get("/", async (req, res) => {
   const posts = await Post.find({});
   res.render("index", {
     posts
   });
 });
-// Fetch Data from database
-
-// // Requests will be here
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "pages/index.html"));
-// });
-
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
-// Renders the home page
 
 app.get("/posts/new", (req, res) => {
   res.render("create");
-  // will go to the views and find the create edge file
 });
+let image = "";
+app.post("/posts/blogz", (req, res) => {
+  // image = req.files;
+  // console.log(image);
+
+  // image.mv(path.resolve(__dirname, "public/posts", image.name), error => {
+  //   Post.create(
+  //     {
+  //       ...req.body,
+  //       image: `/posts/${image.name}`
+  //     },
+  //     (error, post) => {
+  //       res.redirect("/");
+  //     }
+  //   );
+  // });
+  console.log("we are inside the store ");
+});
+
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
 });
-// render the about page
+
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
 });
-// render the contact page
 
 app.get("/post/:id", async (req, res) => {
   const post = await Post.findById(req.params.id);
-  // Get the current item id ( which is like number on the database )
   res.render("post", {
     post
   });
 });
-
-app.post("/posts/store", (req, res) => {
-  // posting information to the /posts/store route
-  console.log(req.body);
-  Post.create(req.body, (error, post) => {
-    res.redirect("/");
-  });
-  // About to create a post
-});
-
-// Database
-mongoose
-  .connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true })
-  .then(() => "You are now connected to Mongo!")
-  .catch(err => console.error("Something went wrong", err));
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
