56bfb6fddae5bf4e55614fe9f3637621f11a8072
giordifungula <jodi.fungula@gmail.com>
2019-07-24 21:34:01 +0200

    session added to users

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
 
 const createPostController = require("./controllers/createPost");
 const homePageController = require("./controllers/homePage");
@@ -22,7 +23,9 @@ const loginUserController = require("./controllers/loginUser");
 const app = new express();
 app.use(
   expressSession({
-    secret: "secret"
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
   })
 );
 // session being used for the users
@@ -31,6 +34,19 @@ mongoose
   .connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true })
   .then(() => "You are now connected to Mongo!")
   .catch(err => console.error("Something went wrong", err));
+
+const mongoStore = connectMongo(expressSession);
+
+app.use(
+  expressSession({
+    secret: "secret",
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection
+    }),
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 app.use(fileUpload());
 app.use(express.static("public"));
